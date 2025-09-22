db4c4657dc0f70371453e9486729a46be3c6f4d2
Emily Brick <brick.emily@gmail.com>
2025-09-22 13:42:14 -0400

    global nav images

--- src/assets/Work/Global_navigation.jsx
+++ src/assets/Work/Global_navigation.jsx
@@ -1,5 +1,14 @@
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import navWireframes from '@/assets/images/globalNavigation_wireframes.png';
+import navHighFidelity1 from '@/assets/images/globalNavigation_highFidelity1.png';
+import navHighFidelity2 from '@/assets/images/globalNavigation_highFidelity2.png';
+import navHighFidelity3 from '@/assets/images/globalNavigation_highFidelity3.png';
+import navResponsive1 from '@/assets/images/globalNavigation_responsive1.png';
+import navResponsive2 from '@/assets/images/globalNavigation_responsive2.png';
+import navResponsive3 from '@/assets/images/globalNavigation_responsive3.png';
+import navResponsive4 from '@/assets/images/globalNavigation_responsive4.png';
+import navVision from '@/assets/images/globalNavigation_vision.png';
 
 function GlobalNavigation() {
   const [count, setCount] = useState(0);
@@ -36,12 +45,88 @@ function GlobalNavigation() {
         <h1 className='mt-4 mb-1 text-title'>GitHub's Global Navigation</h1>
         <p className='mb-6 text-subtitle'>Explorations to launch a new vision for navigation for GitHub</p>
       </div>
+      <div className='max-w-4xl'>
+        <a href={navVision} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navVision} 
+            width={800}
+            alt="Global Navigation Vision"
+            className="w-full h-auto mb-4"
+          />
+        </a>
+      </div>
       <div className='text-left max-w-2xl'>
         <div className='space-y-4 mb-4'>
           <p>I took on the role in early 2019 of iterating on the GitHub Global Nav as part of a larger overhaul to rethink GitHub’s platform. Our goal was to consider GitHub an operating system that works together.</p>
           <p>The initial explorations involved rethinking GitHub’s double stacked tab navigation on popular products such as Pull Requests and Issues, and including a side navigation for easy access to high priority items. We wanted to create a “compact” version of the navigation that allowed users to focus on the task at hand, while still having an easy way to navigate around the product. This was a multi-prong approach, which worked in concert with the Github Command Palette.</p>
-          <p>We worked through and tested several iterations before I left on maternity leave in 2020. The GitHub navigation went through rigorous amounts of iterations and several designers through the years, but I’m proud to say what eventually shipped took some of the core elements and ideas of our explorations. My role involved early exploration, iteration, user testing, and eventually managing the designer who took on the initial beta that shipped to customers in late 2021.</p>
+          <p>We worked through and tested several iterations before I left on maternity leave in 2020. The GitHub navigation went through rigorous amounts of iterations and several designers through the years, but I’m proud to say what eventually shipped took some of the core elements and ideas of our explorations. My role involved early exploration, iteration, and user testing to validate concepts. When I returned from maternity leave, I began managing the designer who took on one of the earlier betas that shipped to customers in late 2021.</p>
         </div>
+      </div>
+      <div className='max-w-4xl'>
+        <a href={navHighFidelity1} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navHighFidelity1} 
+            width={800}
+            alt="Global Navigation High Fidelity"
+            className="w-full h-auto mb-4"
+          />
+        </a>
+        <a href={navHighFidelity2} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navHighFidelity2} 
+            width={800}
+            alt="Global Navigation High Fidelity"
+            className="w-full h-auto mb-4"
+          />
+        </a>
+        <a href={navHighFidelity3} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navHighFidelity3} 
+            width={800}
+            alt="Global Navigation High Fidelity"
+            className="w-full h-auto mb-4"
+          />
+        </a>
+        <a href={navWireframes} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navWireframes} 
+            width={800}
+            alt="Global Navigation Wireframes"
+            className="w-full h-auto mb-4"
+          />
+        </a>
+        <a href={navResponsive1} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navResponsive1} 
+            width={800}
+            alt="Global Navigation Responsive"
+            className="w-full h-auto mb-4"
+          />
+        </a>
+        <a href={navResponsive2} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navResponsive2} 
+            width={800}
+            alt="Global Navigation Responsive"
+            className="w-full h-auto mb-4"
+          />
+        </a>
+        <a href={navResponsive3} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navResponsive3} 
+            width={800}
+            alt="Global Navigation Responsive"
+            className="w-full h-auto mb-4"
+          />
+        </a>
+        <a href={navResponsive4} target="_blank" rel="noopener noreferrer">
+          <img
+            src={navResponsive4} 
+            width={800}
+            alt="Global Navigation Responsive"
+            className="w-full h-auto mb-4"
+          />
+        </a>
       </div>
     </main>
   </div>
