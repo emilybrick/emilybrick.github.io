b34e7ad6c988d35422b843ce1cd3d1e2a847eac3
Emily Brick <brick.emily@gmail.com>
2025-09-25 12:31:12 -0400

    adjusted margin

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="w-full py-6 px-4 text-center text-sm text-gray-500 mt-12">
+    <footer className="w-full py-6 px-4 text-center text-sm text-gray-500 mt-6">
       This <a href="https://github.com/emilybrick/emilybrick.github.io" target="_blank" rel="noopener noreferrer" className="!underline">site</a> was made with <a href="https://github.com/vitejs/vite" target="_blank"className="!underline">Vite</a>, <a href="https://www.tailwindcss.com/" target="_blank"className="!underline">Tailwind</a>, <a href="https://pages.github.com/" target="_blank"className="!underline">GitHub Pages</a>, and 💜. Open-source makes the internet go round.
     </footer>
   );
